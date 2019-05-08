1d79375a0c35a75f1691afaeef4d69abc38efe3b
Wenjing Liu <lwj@Wenjings-Air.princeton.epam.com>
2019-05-08 11:19:06 +0800

    If this commit applies it will change 1. add excises of String

---
+++ week-01/day-03/strings/reverse.py
@@ -0,0 +1,11 @@
+# Create a function called 'reverse' which takes a string as a parameter
+# The function reverses it and returns with the reversed string
+
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+def reverse(toReveerse):
+  return toReveerse[::-1]
+
+print(reverse(reversed))
+
---
+++ week-01/day-03/strings/url_fixer.py
@@ -0,0 +1,10 @@
+# Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+# Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+url = 'https//www.reddit.com/r/nevertellmethebots'
+
+index = url.find('//')
+url = url[:index] + ':' + url[index:]
+
+print(url)
+
